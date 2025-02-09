c6ae3811952fc680076786dbeddedf5f822f4e95
Fares-440 <87226047+Fares-440@users.noreply.github.com>
2025-02-09 21:13:15 +0300

    edit cities seed

--- YdentalDash2/database/seeders/CitiesTableSeeder.php
+++ YdentalDash2/database/seeders/CitiesTableSeeder.php
@@ -8,6 +8,17 @@ class CitiesTableSeeder extends Seeder
 {
     public function run()
     {
-        City::factory()->times(7)->create();
+        $cities = [
+            ['name' => 'صنعاء'],
+            ['name' => 'تعز'],
+            ['name' => 'عدن'],
+            ['name' => 'ذمار'],
+            ['name' => 'إب'],
+            ['name' => 'عمران'],
+        ];
+
+        foreach ($cities as $city) {
+            City::create($city);
+        }
     }
 }
